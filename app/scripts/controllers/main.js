a6d077f61ca201a18ba9f3064e6c75f5cbf6a589
Boo-urns <coreyaburns@gmail.com>
2014-01-24 16:15:29 -0600

    Setup Initial View, working on the form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,33 @@
 'use strict';
 
-angular.module('hitTargetGravityApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+// var app = angular.module('hitTargetGravityApp', []);
+
+// app.config(function ($routeProvider) {
+// 	$routeProvider
+// 		.when('/', {
+// 			templateUrl: 'views/main.html'
+// 		});
+// });
+
+// app.controller('MainCtrl', function ($scope) {
+//     $scope.awesomeThings = [
+//       'HTML5 Boilerplate',
+//       'AngularJS',
+//       'Karma'
+//     ];
+// });
+ angular.module('hitTargetGravityApp', ['ngRoute'])
+  .config(function($routeProvider) {
+  	$routeProvider
+  		.when('/', {
+  			templateUrl: 'views/main.html'
+  		});
   });
+
+function MainCtrl ($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Karma'
+  ];
+};
