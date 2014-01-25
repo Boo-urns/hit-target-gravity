07d6c4d0761e9764a9dd218acd644c768e8626b9
Corey Burns <coreyaburns@gmail.com>
2014-01-25 13:57:24 -0600

    Setup watchCollection for userData

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,33 +1,33 @@
 'use strict';
 
-// var app = angular.module('hitTargetGravityApp', []);
-
-// app.config(function ($routeProvider) {
-// 	$routeProvider
-// 		.when('/', {
-// 			templateUrl: 'views/main.html'
-// 		});
-// });
-
-// app.controller('MainCtrl', function ($scope) {
-//     $scope.awesomeThings = [
-//       'HTML5 Boilerplate',
-//       'AngularJS',
-//       'Karma'
-//     ];
-// });
  angular.module('hitTargetGravityApp', ['ngRoute'])
   .config(function($routeProvider) {
   	$routeProvider
   		.when('/', {
-  			templateUrl: 'views/main.html'
+  			templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
   		});
   });
 
+
 function MainCtrl ($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Karma'
-  ];
+
+  $scope.userData = {};
+
+  // Setting up a default for now
+  $scope.userData = {
+    target: 1.080,
+    collected: 6.5,
+    batchSize: 5
+  }
+  $scope.dme = [
+              {dme: 'Briess Pilsen Light', ppg: 44},
+              {dme: 'Briess Golden Light', ppg: 43},
+              {dme: 'Muntons Light', ppg: 44},
+              {dme: 'Muntons Extra Light', ppg: 37}
+            ];
+
+  $scope.$watchCollection('userData', function(newValues){
+    console.log($scope.userData);
+  });
 };
